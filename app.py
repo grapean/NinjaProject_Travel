16752722ebaf64efeb213778d5bba15bd3b96d09
anseoyeong <anseuyoung@gmail.com>
2021-08-26 00:14:22 +0900

    !?

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ def home():
 def page():
     return render_template('index_page.html')
 
+@app.route('/create')
+def create():
+    return render_template('index_create.html')
+
 @app.route('/comment', methods=['GET'])
 def read_reviews():
     comments = list(db.travel.find({}, {'_id': False}))
@@ -40,6 +44,39 @@ def test_post():
    db.travel.insert_one(doc)
 
    return jsonify({'msg': '댓글 작성 완료!'})
+
+
+@app.route('/create', methods=['POST'])
+def test_create():
+    title_receive = request.form['title_give']
+    season_receive = request.form['season_give']
+    area_receive = request.form['area_give']
+    keywords_receive = request.form['keywords_give']
+    review_text_receive = request.form['review_text_give']
+
+    today = datetime.now()
+    subdata = today.strftime('%Y.%m.%d %H:%M')
+
+    file = request.files["file_give"]
+    extension = file.filename.split('.')[-1]
+    filename = f'file-{subdata}'
+    save_to = f'static/{filename}.{extension}'
+    file.save(save_to)
+
+    doc = {
+        'title': title_receive,
+        'season': season_receive,
+        'area': area_receive,
+        'keywords': keywords_receive,
+        'review_text': review_text_receive,
+        'data': subdata,
+        'file': f'{filename}.{extension}'
+    }
+
+    db.travelreview.insert_one(doc)
+
+    return jsonify({'msg': '리뷰 등록 완료!'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
