0294c8dbd9ed25a69d3c8a05cd576d1e2c9f2efa
anseoyeong <anseuyoung@gmail.com>
2021-08-26 23:42:29 +0900

    셀렉트 박스 삭제

--- app.py
+++ app.py
@@ -78,6 +78,12 @@ def test_create():
     return jsonify({'msg': '리뷰 등록 완료!'})
 
 
+@app.route('/page', methods=['GET'])
+def view_reviews():
+    reviews = list(db.travelreview.find({}, {'_id': False}))
+    return jsonify({'all_reviews': reviews})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
