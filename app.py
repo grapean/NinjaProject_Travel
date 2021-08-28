b8698f57fb0332db35ed7d52d77bfe4cef59eaf0
anseoyeong <anseuyoung@gmail.com>
2021-08-28 23:48:13 +0900

    파일 업로드 기능 테스트 할 때 사용할 업데이트 버전!

--- app.py
+++ app.py
@@ -3,18 +3,51 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 from datetime import datetime
 
+
 @app.route('/')
 def home():
     return render_template('index_home.html')
 
+
+@app.route('/page')
+def page():
+    return render_template('index_page.html')
+
+
 @app.route('/create')
 def create():
     return render_template('index_create.html')
+
+
+@app.route('/comment', methods=['GET'])
+def read_reviews():
+    comments = list(db.travel.find({}, {'_id': False}))
+    return jsonify({'all_comments': comments})
+
+
+@app.route('/page', methods=['POST'])
+def test_post():
+    user_name_receive = request.form['user_name_give']
+    comment_receive = request.form['comment_give']
+
+    today = datetime.now()
+    mytime = today.strftime('%Y.%m.%d %H:%M')
+
+    doc = {
+        'user_name': user_name_receive,
+        'comment': comment_receive,
+        'data': mytime
+    }
+
+    db.travel.insert_one(doc)
+
+    return jsonify({'msg': '댓글 작성 완료!'})
 
 
 @app.route('/create', methods=['POST'])
@@ -25,14 +58,8 @@ def test_create():
     keywords_receive = request.form['keywords_give']
     review_text_receive = request.form['review_text_give']
 
-    file = request.files['file_give']
-    extension = file.filename.split('.')[-1]
     today = datetime.now()
-    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
-    filename = f'file-{mytime}'
-    save_to = f'static/{filename}.{extension}'
-    file.save(save_to)
-
+    mytime = today.strftime('%Y.%m.%d %H:%M')
 
     doc = {
         'title': title_receive,
@@ -40,7 +67,6 @@ def test_create():
         'area': area_receive,
         'keywords': keywords_receive,
         'review_text': review_text_receive,
-        'file': f'{filename}.{extension}',
         'data': mytime
     }
     db.travelreview.insert_one(doc)
@@ -48,8 +74,11 @@ def test_create():
     return jsonify({'msg': '리뷰 등록 완료!'})
 
 
+@app.route('/home', methods=['GET'])
+def show_reviews():
+    reviews = list(db.travelreview.find({}, {'_id': False}))
+    return jsonify({'all_reviews': reviews})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-
-
